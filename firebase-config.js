9bf1be5e9fbbd1ae7fc86da20cfaf88227c654ca
efromatika <ziosinaga456@gmail.com>
2026-04-21 20:54:03 +0700

    baru

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
 const firebaseConfig = {
-  "apiKey": "AIzaSyDJF0DSEOC78eotfKQ7lqYHzZ8ImWA3eSo",
-  'authDomain': "himatika-91e47.firebaseapp.com",
-  'projectId': 'himatika-91e47',
-  "storageBucket": "himatika-91e47.firebasestorage.app",
-  "messagingSenderId": '81243402605',
-  'appId': "1:81243402605:web:8d7c57efebadf0fd5fea64",
-  'measurementId': 'G-E55YJ3Q4WX'
+    "apiKey": "AIzaSyDJF0DSEOC78eotfKQ7lqYHzZ8ImWA3eSo",
+    'authDomain': "himatika-91e47.firebaseapp.com",
+    'projectId': 'himatika-91e47',
+    "storageBucket": "himatika-91e47.firebasestorage.app",
+    "messagingSenderId": '81243402605',
+    'appId': "1:81243402605:web:8d7c57efebadf0fd5fea64",
+    'measurementId': 'G-E55YJ3Q4WX'
 };
 
 if (typeof firebase === "undefined") {
-  console.warn("Firebase SDK not loaded. Make sure firebase-app-compat and firebase-auth-compat are included in index.html");
+    console.warn("Firebase SDK not loaded. Make sure firebase-app-compat and firebase-auth-compat are included in index.html");
 } else {
-  try {
-    firebase.initializeApp(firebaseConfig);
-  } catch (_0x27efbd) {
-    console.error("Error initializing Firebase:", _0x27efbd);
-  }
+    try {
+        firebase.initializeApp(firebaseConfig);
+    } catch (err) {
+        console.error("Error initializing Firebase:", err);
+    }
 }
